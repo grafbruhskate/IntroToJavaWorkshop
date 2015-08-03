e9adc25315a3f556674b0b46fa845bb82a0fec0e
league-workshopper-1 <Guest@wts9.attlocal.net>
2015-08-03 15:01:54 -0700

    THERES A SPY OVER THERE

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,14 +1,19 @@
 package day1.robot;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-		new Robot();
-		
-		
-	}
+Robot r2d2 = new Robot();
+for (int i = 0; i < 4; i++) {
+r2d2.setPenColor(Color.MAGENTA);
+r2d2.penDown();
+r2d2.move(200);
+r2d2.move(200);
+r2d2.turn(180);
 }
+}}
